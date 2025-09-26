fb9dd3624812da404ae367e88033d5940a5b9eb4
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2025-09-26 17:07:33 +0200

    remove placeholder

--- src/DNSPacket.ts
+++ src/DNSPacket.ts
@@ -1,11 +1,3 @@
 export class DNSPacket {
-  private readonly name: string;
-  private readonly type: RRType;
-  private readonly class: RRClass;
-  private readonly ttl: number;
-  private readonly rdLength: number;
-  private readonly rData: string;
-
-
-}
+} 
  
