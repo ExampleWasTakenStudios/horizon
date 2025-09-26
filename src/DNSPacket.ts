501983ea31014dfb7c3d211cc2da6d1ef1115402
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2025-09-26 16:07:10 +0200

    save and push

---
+++ src/DNSPacket.ts
@@ -0,0 +1,11 @@
+export class DNSPacket {
+  private readonly name: string;
+  private readonly type: RRType;
+  private readonly class: RRClass;
+  private readonly ttl: number;
+  private readonly rdLength: number;
+  private readonly rData: string;
+
+
+}
+ 
---
+++ src/interfaces/QType.ts
@@ -0,0 +1,7 @@
+export interface QType {
+  AXFR: 252,
+  MAILB: 253
+  MAILA: 254,
+  WILD: 255,
+}
+ 
