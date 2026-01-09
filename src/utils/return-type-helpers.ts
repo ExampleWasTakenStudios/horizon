b3a068367e15c752c94f5c3fae8ce9f1fe6d2d00
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2026-01-09 23:41:15 +0100

    merge ReturnResult types and implementation

--- src/utils/return-type-helpers.ts
+++ src/utils/return-type-helpers.ts
@@ -1,5 +1,22 @@
 import type { DNS_RESPONSE_CODES } from '../modules/transport-layer-subsystem/wire-protocol/DNS-core/constants/DNS_RESPONSE_CODES.js';
-import type { ReturnFailure, ReturnSuccess } from '../types/ReturnResult.js';
+
+interface ErrArgs {
+  rCode: DNS_RESPONSE_CODES;
+  debugInfo: unknown;
+}
+
+export interface ReturnSuccess<T> {
+  success: true;
+  data: T;
+}
+
+export interface ReturnFailure {
+  success: false;
+  rCode: DNS_RESPONSE_CODES;
+  debugInfo: unknown;
+}
+
+export type ReturnResult<T> = ReturnSuccess<T> | ReturnFailure;
 
 /**
  * Constructs a {@link ReturnSuccess} object.
@@ -10,11 +27,6 @@ export const ok = <T>(data: T): ReturnSuccess<T> => {
     data: data,
   };
 };
-
-interface ErrArgs {
-  rCode: DNS_RESPONSE_CODES;
-  debugInfo: unknown;
-}
 
 /**
  * Constructs a {@link ReturnFailure} object.
