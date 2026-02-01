d9ef18ff0c0c61567e8e03fcce2cfe1a2be2d83f
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2026-02-01 19:15:10 +0100

    add bundler script

---
+++ scripts/bundle.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-console */
+import dotenv from 'dotenv';
+import * as esbuild from 'esbuild';
+import { execSync } from 'node:child_process';
+import fs from 'node:fs';
+import path from 'node:path';
+
+const str = JSON.stringify;
+
+const envConfig = dotenv.config({ debug: true, path: './prod.env', quiet: true }).parsed;
+
+if (!envConfig) {
+  console.error(`
+    ❌ .env file not found.
+  `);
+  process.exit(1);
+}
+
+const ENTRY_FILE = './out/compiled/index.js';
+const COMMIT_HASH = execSync('git rev-parse --short HEAD').toString().trim();
+const OUT_DIR = './out/';
+const OUT_FILE = 'horizon.js';
+
+if (!fs.existsSync(ENTRY_FILE)) {
+  console.error(`
+    ❌ Entry file ${ENTRY_FILE} not found.
+  `);
+  process.exit(1);
+}
+
+const define = {
+  'process.env.NODE_ENV': str(envConfig.NODE_ENV),
+  'process.env.HORIZON_VERSION': str(envConfig.HORIZON_VERSION),
+  'process.env.HORIZON_COMMIT_HASH': str(COMMIT_HASH),
+  'process.env.HORIZON_CONSOLE_LOG_LEVEL': str(envConfig.HORIZON_CONSOLE_LOG_LEVEL),
+  'process.env.HORIZON_FILE_LOG_LEVEL': str(envConfig.HORIZON_FILE_LOG_LEVEL),
+};
+
+const _start = performance.mark('start_build');
+await esbuild.build({
+  entryPoints: [ENTRY_FILE],
+  bundle: true,
+  minify: true,
+  platform: 'node',
+  target: 'node25',
+  outfile: path.join(OUT_DIR, OUT_FILE),
+  format: 'esm',
+  define: define,
+  banner: {
+    js: "import { createRequire } from 'module'; const require = createRequire(import.meta.url);",
+  },
+});
+const _end = performance.mark('end_build');
+const measure = performance.measure('bundle', { start: 'start_build', end: 'end_build' });
+
+console.log(`✅ Bundle complete in ${Math.round(measure.duration)} ms`);
