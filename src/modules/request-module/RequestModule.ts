29eb7216613833b1daa1eb568db6e296bc546f17
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2026-02-01 02:49:42 +0100

    fix wrong ID for inflight query keys and log statements

--- src/modules/request-module/RequestModule.ts
+++ src/modules/request-module/RequestModule.ts
@@ -10,6 +10,10 @@ export class RequestModule extends Module {
   private readonly resolutionSubsystem: ResolutionSubsystem;
   private readonly transportLayer: TransportLayerSubsystem;
 
+  /**
+   * @param K horizonID
+   * @param V `InflightQuery`
+   */
   private readonly inflightQueries: Map<number, InflightQuery>;
   private internalIdCounter: number;
 
@@ -57,7 +61,10 @@ export class RequestModule extends Module {
     const originalId = data.buf.readUInt16BE(0);
     const horizonId = this.generateId();
 
-    const timeout = setTimeout(() => this.inflightQueries.delete(originalId), queryTimeout);
+    const timeout = setTimeout(() => {
+      this.logger.verbose(`Inflight query ${originalId.toString()} (Horizon ID: ${horizonId.toString()}) timed out.`);
+      this.inflightQueries.delete(originalId);
+    }, queryTimeout * 1000);
 
     const inflight: InflightQuery = {
       originalId,
@@ -68,7 +75,7 @@ export class RequestModule extends Module {
       timeout,
     };
 
-    this.inflightQueries.set(originalId, inflight);
+    this.inflightQueries.set(horizonId, inflight);
 
     // Write horizon's ID to the buffer before sending it to resolution subsystem
     data.buf.writeUint16BE(horizonId, 0);
@@ -81,30 +88,35 @@ export class RequestModule extends Module {
    * @param data Returning data.
    */
   private handleReturning(data: ReceivedData): void {
+    this.logger.debug('Received returning data.');
     // Buffer is way too short to be a valid DNS message.
     // We check for length == 2 to be sure we can read the transaction ID safely.
     if (data.buf.length < 2) {
+      this.logger.debug('Data buffer was shorter than 2. -> Returning...');
       return;
     }
 
-    const id = data.buf.readUint16BE(0);
-    const inflight = this.inflightQueries.get(id);
+    const horizonId = data.buf.readUint16BE(0);
+    const inflight = this.inflightQueries.get(horizonId);
 
     // Check if the received data corresponds to an inflight query.
     if (!inflight) {
+      this.logger.debug('No matching query in flight. -> Returning...');
       return;
     }
 
     // Land query
-    this.inflightQueries.delete(id);
+    this.logger.debug('Landing query...');
+    clearTimeout(inflight.timeout);
+    this.inflightQueries.delete(horizonId);
 
     // Write original transaction ID back to the buffer.
+    this.logger.debug('Rewriting transaction ID to original ID...');
     data.buf.writeUInt16BE(inflight.originalId, 0);
 
     // Send answer to client
+    this.logger.debug('Sending buffer to ', inflight.clientIP, ':', inflight.clientPort);
     this.transportLayer.getDownstreamModule().send(data.buf, inflight.clientIP, inflight.clientPort);
-
-    clearTimeout(inflight.timeout);
   }
 
   private generateId(): number {
