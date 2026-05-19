45a9b48db0acada2a2419499f97d690cb5977c07
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2026-05-19 22:02:00 +0200

    add packet buffer

---
+++ src/buffer.rs
@@ -0,0 +1,49 @@
+pub struct PacketBuffer {
+    buffer: [u8; 512],
+    position: usize,
+}
+
+/// Represents a cursored buffer with length 512.
+/// It features special methods to read the buffer that keep track of the cursor i.e. the current location in the buffer.
+impl PacketBuffer {
+    pub fn new() -> Self {
+        PacketBuffer {
+            buffer: [0; 512],
+            position: 0,
+        }
+    }
+
+    pub fn get_buffer(&self) -> [u8; 512] {
+        self.buffer
+    }
+
+    pub fn get_position(&self) -> usize {
+        self.position
+    }
+
+    pub fn read_u8(&mut self) -> Result<u8, &'static str> {
+        if self.position >= 512 {
+            return Err("End of buffer");
+        }
+
+        let value = self.buffer[self.position];
+        self.position += 1;
+
+        Ok(value)
+    }
+
+    pub fn read_u16(&mut self) -> Result<u16, &'static str> {
+        let value = ((self.read_u8()? as u16) << 8) | (self.read_u8()? as u16);
+
+        Ok(value)
+    }
+
+    pub fn read_u32(&mut self) -> Result<u32, &'static str> {
+        let value = ((self.read_u8()? as u32) << 24)
+            | ((self.read_u8()? as u32) << 16)
+            | ((self.read_u8()? as u32) << 8)
+            | ((self.read_u8()? as u32) << 0);
+
+        Ok(value)
+    }
+}
