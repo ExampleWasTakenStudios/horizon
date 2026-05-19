cfd4284df477f53d87114497ff6f33bc029548f2
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2026-05-19 22:02:08 +0200

    add lib.rs and main.rs

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+use std::process::exit;
+
+pub mod protocol;
+pub mod buffer;
+
+pub fn entry() {
+    eprintln!("Program is not yet ready to run. Exiting...");
+    exit(0);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use horizon::entry;
+
 fn main() {
-    println!("Hello, world!");
+    entry();
 }
