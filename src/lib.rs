e04975fcd9da6f31278d49028dfae0581d2c0609
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2026-05-20 19:25:31 +0200

    add basic network functionality
    * prints the received network data

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,41 @@
-use std::process::exit;
+use std::io::Write;
+
+use tokio::{self, net, runtime};
+
+use crate::{buffer::PacketBuffer, protocol::DnsPacket};
 
 pub mod protocol;
 pub mod buffer;
 
 pub fn entry() {
-    eprintln!("Program is not yet ready to run. Exiting...");
-    exit(0);
+    let rt = runtime::Builder::new_multi_thread()
+        .enable_all()
+        .name("horizon-tokio-runtime")
+        .thread_name("horizon-worker")
+        .build()
+        .unwrap();
+
+    rt.block_on(async {
+        let socket = net::UdpSocket::bind("0.0.0.0:0").await.unwrap();
+        println!("Bound to socket: {:?}", socket);
+        let mut buffer: [u8; 512] = [0; 512];
+
+
+        loop {
+             let (bytes, source) = match socket.recv_from(&mut buffer).await {
+                Ok(result) => result,
+                Err(e) => {
+                    eprintln!("Failure while receiving datagram...should send a SERVFAIL response code.\n{:#?}", e);
+                    continue;
+                }
+             };
+
+             tokio::spawn(async move {
+                let mut buffer = PacketBuffer::from_raw_buffer(buffer);
+                let packet = DnsPacket::parse_from(&mut buffer).unwrap();
+
+                println!("Received {} bytes from {} in packet: {:#?}", bytes, source, packet)
+             });
+        }
+    })
 }
