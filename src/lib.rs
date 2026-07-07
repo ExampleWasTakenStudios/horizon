c34ce1b6a8ed6ee1e34a83c017f3bb4793744dc5
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2026-07-08 01:29:28 +0200

    IES build test implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 use tokio::{self, runtime};
 
-use crate::isc::init_msg_channels;
+use crate::{
+    isc::init_msg_channels,
+    systems::{IesChannels, IngressEgressSystem},
+};
 
 mod buffer;
+mod isc;
 mod protocol;
 mod query_state;
 mod systems;
-mod isc;
 
 pub const IP_ADDR: &str = "0.0.0.0:1234";
 pub const MAX_PACKET_SIZE: usize = 1232;
@@ -26,6 +29,16 @@ pub fn entry() {
         // Configure main socket
         let socket = tokio::net::UdpSocket::bind(IP_ADDR).await.unwrap();
 
+        // Start IES
+        IngressEgressSystem::run(IesChannels {
+            ies_ingress_rx: channels.ies_ingress_channel.rx,
+            ies_to_dps_tx: channels.ies_to_dps.tx,
+            ies_egress_tx: channels.ies_egress_channel.tx,
+            ies_resolving_rx: channels.ies_resolving_channel.rx,
+            ies_answer_rx: channels.ies_answer_channel.rx,
+        })
+        .await;
+
         loop {
             let mut buf = [0_u8; MAX_PACKET_SIZE];
             let (_, origin) = match socket.recv_from(&mut buf).await {
@@ -35,8 +48,6 @@ pub fn entry() {
                 }
                 Ok(value) => value,
             };
-
-            
         }
     })
 }
--- src/systems/ies.rs
+++ src/systems/ies.rs
@@ -13,20 +13,29 @@ pub struct NetworkTrafficMessage {
     data: [u8; MAX_PACKET_SIZE],
 }
 
+pub struct IesChannels {
+    pub ies_ingress_rx: Receiver<NetworkTrafficMessage>,
+    pub ies_egress_tx: Sender<NetworkTrafficMessage>,
+    pub ies_to_dps_tx: Sender<QueryState>,
+    pub ies_resolving_rx: Receiver<HalfDuplexMessage<DnsPacket, Option<DnsPacket>>>,
+    pub ies_answer_rx: Receiver<QueryState>,
+}
+
 pub struct IngressEgressSystem;
 
 impl IngressEgressSystem {
-    pub async fn run(
-        mut incoming_rx: Receiver<NetworkTrafficMessage>,
-        dps_tx: Sender<QueryState>,
-        mut srs_resolving_rx: Receiver<HalfDuplexMessage<DnsPacket, Option<DnsPacket>>>,
-        mut answer_rx: Receiver<QueryState>,
-        outgoing_tx: Sender<NetworkTrafficMessage>,
-    ) {
+    pub async fn run(channels: IesChannels) {
+        println!("Starting IES...");
+
+        let mut ingress_rx = channels.ies_ingress_rx;
+        let mut answer_rx = channels.ies_answer_rx;
+        let mut resolving_rx = channels.ies_resolving_rx;
+
         // This task handles all incoming queries.
         tokio::spawn(async move {
+            println!("Starting incoming query task...");
             loop {
-                let incoming_message = match incoming_rx.recv().await {
+                let incoming_message = match ingress_rx.recv().await {
                     None => {
                         panic!("IES RX channel to receive incoming traffic closed unexpectedly.")
                     }
@@ -44,12 +53,13 @@ impl IngressEgressSystem {
                 let query_state = QueryState::new(incoming_message.origin, packet);
 
                 // Send DnsPacket to DPS
-                dps_tx.send(query_state).await;
+                channels.ies_to_dps_tx.send(query_state).await;
             }
         });
 
         // This task receives answered query states and sends them back to the querying client.
         tokio::spawn(async move {
+            println!("Starting answer task...");
             loop {
                 let received_query_state = match answer_rx.recv().await {
                     None => {
@@ -68,7 +78,8 @@ impl IngressEgressSystem {
                 };
 
                 // Send data to socket
-                outgoing_tx
+                channels
+                    .ies_egress_tx
                     .send(NetworkTrafficMessage {
                         origin: *received_query_state.get_origin(),
                         data: bytes,
@@ -79,67 +90,75 @@ impl IngressEgressSystem {
 
         // This task handles the query forwarding to an upstream resolver and returns it to the SRS.
         tokio::spawn(async move {
-            let message = match srs_resolving_rx.recv().await {
-                None => {
-                    panic!("IES-SRS resolving channel closed unexpectedly.");
-                }
-                Some(v) => v,
-            };
+            println!("Starting upstream forward task...");
+            loop {
+                let message = match resolving_rx.recv().await {
+                    None => {
+                        panic!("IES-SRS resolving channel closed unexpectedly.");
+                    }
+                    Some(v) => v,
+                };
 
-            // Translate packet into bytes
-            let bytes = match message.payload.to_raw_bytes() {
-                None => {
-                    eprintln!("Error while translating query for upstream resolver.");
-                    return;
-                }
-                Some(v) => v,
-            };
-
-            // Spawn socket
-            tokio::spawn(async move {
-                let mut receive_buf = [0_u8; MAX_PACKET_SIZE];
-                let socket = match tokio::net::UdpSocket::bind("0.0.0.0:0").await {
-                    Err(e) => {
-                        eprintln!("Error while trying to bind resolving socket. Error: {e}");
-                        message.return_channel.send(None);
+                // Translate packet into bytes
+                let bytes = match message.payload.to_raw_bytes() {
+                    None => {
+                        eprintln!("Error while translating query for upstream resolver.");
                         return;
                     }
-                    Ok(v) => v,
+                    Some(v) => v,
                 };
 
-                socket.connect("1.1.1.1:53").await;
-                match socket.try_send(&bytes) {
-                    Err(e) => {
-                        eprintln!("Error while trying to send query to upstream resolver. Error: {e}");
+                // Spawn socket
+                tokio::spawn(async move {
+                    let mut receive_buf = [0_u8; MAX_PACKET_SIZE];
+                    let socket = match tokio::net::UdpSocket::bind("0.0.0.0:0").await {
+                        Err(e) => {
+                            eprintln!("Error while trying to bind resolving socket. Error: {e}");
+                            message.return_channel.send(None);
+                            return;
+                        }
+                        Ok(v) => v,
+                    };
+
+                    if let Err(e) = socket.connect("1.1.1.1:53").await {
+                        eprintln!(
+                            "Error while trying to send query to upstream resolver. Error: {e}"
+                        );
+                    };
+                    if let Err(e) = socket.try_send(&bytes) {
+                        eprintln!(
+                            "Error while trying to send query to upstream resolver. Error: {e}"
+                        );
                         message.return_channel.send(None);
                         return;
-                    },
-                    _ => ()
-                };
+                    };
 
-                match socket.recv(&mut receive_buf).await {
-                    Err(e) => {
-                        eprintln!("Error while receiving data from upstream resolver. Error. {e}");
-                        message.return_channel.send(None);
-                        return;
-                    },
-                    Ok(v) => v,
-                };
+                    match socket.recv(&mut receive_buf).await {
+                        Err(e) => {
+                            eprintln!(
+                                "Error while receiving data from upstream resolver. Error. {e}"
+                            );
+                            message.return_channel.send(None);
+                            return;
+                        }
+                        Ok(v) => v,
+                    };
 
-                // Parse received data into DnsPacket
-                let mut packet_buf = PacketBuffer::from_raw_buffer(receive_buf);
-                let received_packet = match DnsPacket::parse_from(&mut packet_buf) {
-                    Err(_) => {
-                        eprintln!("Error while parsing received upstream data.");
-                        message.return_channel.send(None);
-                        return;
-                    },
-                    Ok(v) => v,
-                };
+                    // Parse received data into DnsPacket
+                    let mut packet_buf = PacketBuffer::from_raw_buffer(receive_buf);
+                    let received_packet = match DnsPacket::parse_from(&mut packet_buf) {
+                        Err(_) => {
+                            eprintln!("Error while parsing received upstream data.");
+                            message.return_channel.send(None);
+                            return;
+                        }
+                        Ok(v) => v,
+                    };
 
-                // Send data back to SRS
-                message.return_channel.send(Some(received_packet));
-            });
+                    // Send data back to SRS
+                    message.return_channel.send(Some(received_packet));
+                });
+            }
         });
     }
 }
