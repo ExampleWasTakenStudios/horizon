4673669632aa3370cf2e0308f28e7e2de95e9ea4
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2026-07-09 20:25:08 +0200

    add timeout field and rename IesCommand struct to IesResolveCommand

--- src/systems/srs.rs
+++ src/systems/srs.rs
@@ -1,9 +1,13 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, time::Duration};
 
 use crate::protocol::packet::DnsPacket;
 
-/// An IES Command is sent to the IES to forward a packet to the specified resolver.
-pub struct IesCommand {
+/// An IES Resolve Command is sent to the IES to forward a packet to the specified resolver.
+///
+/// It contains the `SocketAddr` of the target upstream resolver as well as the timeout after which the IES should
+/// clear the memory associated with the query and the `DnsPacket` representing the query.
+pub struct IesResolveCommand {
     pub resolver_address: SocketAddr,
+    pub timeout: Duration,
     pub packet: DnsPacket,
 }
