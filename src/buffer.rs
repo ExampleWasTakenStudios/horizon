2ddb41b3ed89c9eca3b273be3b54b9c1bbd78b60
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2026-07-12 15:56:53 +0200

    add write methods to PacketBuffer

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,5 +1,8 @@
+use std::io::Write;
+
 use crate::protocol::ResponseCode;
 
+/// A cursored buffer with length `T`.
 pub struct PacketBuffer<const T: usize> {
     buffer: [u8; T],
     position: usize,
@@ -11,8 +14,6 @@ impl Default for PacketBuffer<1232> {
     }
 }
 
-/// Represents a cursored buffer with length 512.
-/// It features special methods to read the buffer that keep track of the cursor i.e. the current location in the buffer.
 impl<const T: usize> PacketBuffer<T> {
     pub fn new() -> Self {
         PacketBuffer {
@@ -78,5 +79,55 @@ impl<const T: usize> PacketBuffer<T> {
         }
 
         Ok(subarray)
+    }
+
+    pub fn write_u8(&mut self, value: u8) -> Result<(), &str> {
+        let v = self
+            .buffer
+            .get_mut(self.position)
+            .ok_or("Index out of bounds.")?;
+        *v = value;
+
+        self.position += 1;
+
+        Ok(())
+    }
+
+    pub fn write_u16(&mut self, value: u16) -> Result<(), &str> {
+        let buf = [(value & 0xFF00) as u8, (value & 0x00FF) as u8];
+
+        let mut v = self
+            .buffer
+            .get_mut(self.position..=(self.position + 1))
+            .ok_or("Index out of bounds.")?;
+        match v.write(&buf) {
+            Err(_) => {
+                return Err("Could not write u16 value to PacketBuffer.");
+            }
+            Ok(length) => self.position += length,
+        };
+
+        Ok(())
+    }
+
+    pub fn write_u32(&mut self, value: u32) -> Result<(), &str> {
+        let buf = [
+            (value & 0xFF0000) as u8,
+            (value & 0x00FF00) as u8,
+            (value & 0x0000FF) as u8,
+        ];
+
+        let mut v = self
+            .buffer
+            .get_mut(self.position..=(self.position + 2))
+            .ok_or("Index out of bounds.")?;
+        match v.write(&buf) {
+            Err(_) => {
+                return Err("Could not write u32 value to PacketBuffer.");
+            }
+            Ok(length) => self.position += length,
+        };
+
+        Ok(())
     }
 }
