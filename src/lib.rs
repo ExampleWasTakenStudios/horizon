2136f5e4218f78acdb12c40b5d27105b0b0aa524
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2026-07-15 02:06:46 +0200

    delete query_state.rs

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,6 @@ use tokio::{net::UdpSocket, runtime, task::JoinSet};
 mod buffer;
 mod protocol;
 mod query;
-mod query_state;
 mod cache;
 
 const DOWNSTREAM_IP_ADDR: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
