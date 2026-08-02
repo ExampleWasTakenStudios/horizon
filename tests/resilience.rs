46d8925334992c4004b1a46da41c689e38de23b1
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2026-08-02 18:32:47 +0200

    prepare test infrastructure

---
+++ tests/resilience.rs
@@ -0,0 +1,5 @@
+/*
+The following things should be tested in this file:
+- Ensure malformed packets, sudden TCP connection drops from the client, etc. are handled gracefully and do not lead to an application crash.
+
+*/
